252c4097db3a7bc9f4ed8a3a0d02745edc1ac5db
arfansshah <arfansshah3@gmail.com>
2025-12-14 17:18:39 +0530

    Buttons

--- Scripts/animations.gd
+++ Scripts/animations.gd
@@ -3,6 +3,11 @@ extends MarginContainer
 @onready var click_button: TextureButton = $CenterContainer/ClickButton
 @onready var indicators: Control = $"../../Indicators"
 @onready var template: Label = $"../../Indicators/Template"
+@export var indicator_scene: PackedScene
+
+
+var cookies_per_click = 1
+
 
 func _ready() -> void:
 	click_button.pivot_offset = click_button.size  / 2
@@ -18,11 +23,12 @@ func _on_click_button_button_up() -> void:
 
 
 func _on_control_pancake_clicked(amount) -> void:
-	var  indicator = template.duplicate()
-	indicator.text = "+" +  str(amount)
-	indicator.position = get_global_mouse_position()
-	indicator.visible = true 
-	indicators.add_child(indicator )
-	indicator.get_child(0).start() 
-	
-	   
+	if indicator_scene == null:
+		return
+
+	var indicator = indicator_scene.instantiate()
+	indicator.text = "+" + str(amount)
+	indicator.global_position = get_global_mouse_position()
+	indicator.visible = true
+	indicators.add_child(indicator)
+	indicator.get_child(0).start()
